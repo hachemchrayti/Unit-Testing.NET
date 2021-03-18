18d95721ad5df8e36de22ebb375f7efe53e5d85f
Hachem Chrayti <hachem.chrayti@outlook.fr>
2021-03-18 18:44:20 +0100

    Exercise DemeritPointsCalculatorTests Unit testing

--- 1.UnitTesting/TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
+++ 1.UnitTesting/TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -5,7 +5,7 @@ using TestNinja.Fundamentals;
 namespace TestNinja.UnitTests
 {
     [TestFixture]
-    public class DemeritPointsCalculatorTests
+    public class DemeritPointsCalculatorTests  
     {
         [Test]
         [TestCase(-1)]
