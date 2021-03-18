ea3425715389a77ad8cd9755636ebdc77eaa614b
Hachem Chrayti <hachem.chrayti@outlook.fr>
2021-03-18 18:11:56 +0100

    Test Method with void return Type

---
+++ 1.UnitTesting/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+   public  class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var errLogger = new ErrorLogger();
+            errLogger.Log("a");
+            Assert.That(errLogger.LastError,Is.EqualTo("a"));
+        }
+    }
+}
