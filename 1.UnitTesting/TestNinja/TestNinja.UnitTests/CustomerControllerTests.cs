c45e42434591a706d636a7148c1d36a24dda480f
Hachem Chrayti <hachem.chrayti@outlook.fr>
2021-03-18 18:06:17 +0100

    Testing the Return Types of methods (in our case Http ok or Not Found)

---
+++ 1.UnitTesting/TestNinja/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+   public class CustomerControllerTests
+   {
+       [Test]
+       public void getCustomer_IdIsZero_ShouldReturnNotFound()
+       {
+           var controller = new CustomerController();
+           var result = controller.GetCustomer(0);
+
+           //result must be notFound object
+           Assert.That(result,Is.TypeOf<NotFound>());
+           //result shoud be NotFound object or one of its derivatives
+           //Assert.That(result, Is.InstanceOf<NotFound>());
+
+        }
+
+       [Test]
+       public void getCustomer_IdIsNOTZero_ShouldReturnOK()
+       {
+           var controller = new CustomerController();
+           var result = controller.GetCustomer(1);
+
+           Assert.That(result, Is.TypeOf<Ok>());
+       }
+    }
+}
